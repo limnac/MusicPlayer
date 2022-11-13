a4b50310cf9981ca08ee9bc10e085850c53daa8f
limnac <carl.hu@live.cn>
2022-11-13 20:35:23 +0800

    播放页界面完善

--- app/src/main/java/com/limnac/musicplayer/ListFragment.java
+++ app/src/main/java/com/limnac/musicplayer/ListFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +60,11 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        Button btn = view.findViewById(R.id.btn_test);
+        btn.setOnClickListener(v->{
+            PlayActivity.startPlayActivity(getActivity());
+        });
+        return view;
     }
 }
---
+++ app/src/main/java/com/limnac/musicplayer/PlayActivity.java
@@ -0,0 +1,55 @@
+package com.limnac.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ImageButton;
+
+public class PlayActivity extends AppCompatActivity {
+
+    private final int REPEAT_MODEL = 0;
+    private final int REPEAR_ONCE_MODEL = 1;
+    private final int SHUFFLE = 2;
+    private int repeat_count = -1;
+    private boolean isPlay = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        ImageButton btn_repeat_model = findViewById(R.id.repeat_model_ibtn);
+        btn_repeat_model.setOnClickListener(v-> changeRepeatModel(btn_repeat_model));
+        ImageButton btn_play_pause = findViewById(R.id.play_pause_ibtn);
+        btn_play_pause.setOnClickListener(v-> play2pause(btn_play_pause));
+    }
+
+    private void play2pause(ImageButton ibtn){
+        if(isPlay){
+            isPlay = false;
+            ibtn.setImageResource(R.drawable.vector_drawable_pause);
+        }else{
+            isPlay = true;
+            ibtn.setImageResource(R.drawable.vector_drawable_play);
+        }
+    }
+
+    private void changeRepeatModel(ImageButton ibtn){
+        repeat_count = (repeat_count+1)%3;
+        if(repeat_count==REPEAT_MODEL){
+            ibtn.setImageResource(R.drawable.vector_drawable_repeatonce);
+        }else if(repeat_count==REPEAR_ONCE_MODEL){
+            ibtn.setImageResource(R.drawable.vector_drawable_shuffle);
+        }else if(repeat_count==SHUFFLE){
+            ibtn.setImageResource(R.drawable.vector_drawable_repeat);
+        }
+    }
+
+    public static void startPlayActivity(Context context){
+        Intent intent = new Intent(context,PlayActivity.class);
+        context.startActivity(intent);
+    }
+}
